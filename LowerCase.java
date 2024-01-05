9ba4576de72b8313e634eab3a477e3b2088e5a56
amitfrumkin <amit.frumkin@post.runi.ac.il>
2024-01-05 11:47:37 +0200

    uniquechars and lowercase

--- LowerCase.java
+++ LowerCase.java
@@ -1,13 +1,22 @@
 
-public class LowerCase {
-	public static void main (String[] args) {
+/** String processing exercise 1. */
+public class LowerCase2 {
+    public static void main(String[] args) {  
+        String str = args[0];
+        System.out.println(lowerCase(str));
+    }
 
-        String user = (args[0]) ; 
+   /**
+    * Returns a string which is identical to the original string, 
+    * except that all the upper-case letters are converted to lower-case letters.
+    * Non-letter characters are left as is.
+    */
+    public static String lowerCase(String s) {
+
         String answer = "" ;
-        int a = user.length() ;
 
-        for(int i = 0 ; i < a ; i++){
-            char letter = user.charAt(i) ;
+        for(int i = 0 ; i < s.length() ; i++){
+            char letter = s.charAt(i) ;
 
             if ((letter >= 'A') && (letter <= 'Z') ){
                 answer = answer + (char) (letter + 32) ;
@@ -15,10 +24,7 @@ public class LowerCase {
                 answer = answer + letter ; 
             }
         }
-        System.out.println(answer) ;
+        
+        return answer ; 
     }
 }
-
-    
-    
-        
--- UniqueChars.java
+++ UniqueChars.java
@@ -5,13 +5,26 @@ public class UniqueChars {
         System.out.println(uniqueChars(str));
     }
 
+
     /**
      * Returns a string which is identical to the original string, 
      * except that all the duplicate characters are removed,
      * unless they are space characters.
      */
     public static String uniqueChars(String s) {
-        // Replace the following statement with your code
-        return null;
+        
+        String answer = "" ;
+
+        for(int i = 0 ; i < s.length() ; i++){
+
+            char letter = s.charAt(i) ;
+            
+            if (letter == ' ' || answer.indexOf(letter) == -1){
+                answer = answer + letter ;
+            }
+        }
+
+
+        return answer ; 
     }
 }
